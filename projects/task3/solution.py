97747b4ba4dbb4e2c422ce62e7de6fc1b84bcfa3
nisutte <nisutte@student.ethz.ch>
2023-11-23 20:34:47 +0100

    task 3 first implementation with gpytorch

--- projects/task3/solution.py
+++ projects/task3/solution.py
@@ -1,8 +1,9 @@
 """Solution."""
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
-# import additional ...
-
+import gpytorch
+import torch
+import matplotlib.pyplot as plt
 
 # global variables
 DOMAIN = np.array([[0, 10]])  # restrict \theta in [0, 10]
@@ -11,11 +12,115 @@ SAFETY_THRESHOLD = 4  # threshold, upper bound of SA
 
 # TODO: implement a self-contained solution in the BO_algo class.
 # NOTE: main() is not called by the checker.
-class BO_algo():
+
+class GP(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, type, iterations=50):
+        # super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        super().__init__(train_x, train_y, likelihood)
+        self.training_iter = iterations
+        self.model_type = type
+
+        k = gpytorch.kernels
+        if type == 'f':
+            self.mean_module = gpytorch.means.ConstantMean()
+            self.covar_module = k.ScaleKernel(k.MaternKernel(nu=2.5))
+            # self.covar_module = k.MaternKernel(nu=2.5)
+        elif type == 'v':
+            self.mean_module = gpytorch.means.ConstantMean()
+            self.covar_module = k.AdditiveKernel(k.LinearKernel(), k.MaternKernel(nu=2.5))
+        else:
+            raise Exception(f"model hast to be 'f' of 'v' but is {type}")
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+    @property
+    def output_scale(self):
+        """Get output scale."""
+        return self.covar_module.outputscale
+
+    @property
+    def length_scale(self):
+        """Get length scale."""
+        return self.covar_module.base_kernel.lengthscale
+
+    @length_scale.setter
+    def length_scale(self, value):
+        self.covar_module.base_kernel.lengthscale = value
+
+
+
+class BOpt(torch.nn.Module):
+    """Abstract Bayesian Optimization class. ."""
+
+    def __init__(self, gp, x, beta=2.0):
+        super().__init__()
+        self.gp = gp
+        self.gp.eval()
+        self.gp.likelihood.eval()
+        self.x = x
+        self.update_acquisition_function(x)
+
+    def update_gp(self, new_inputs, new_targets):
+        """Update GP with new points."""
+        new_inputs, new_targets = torch.FloatTensor(new_inputs), torch.FloatTensor(new_targets)
+        if len(new_targets.shape) == 2:
+            new_targets = new_targets[0]
+        inputs = torch.cat((self.gp.train_inputs[0], new_inputs[0]), dim=0)
+        targets = torch.cat((self.gp.train_targets, new_targets), dim=-1)
+        self.gp.set_train_data(inputs, targets, strict=False)
+        self.update_acquisition_function(new_inputs)
+
+    def get_best_value(self):
+        idx = self.gp.train_targets.argmax()
+        if len(self.gp.train_targets) == 1:
+            xmax, ymax = self.gp.train_inputs[idx], self.gp.train_targets[idx]
+        else:
+            xmax, ymax = self.gp.train_inputs[0][idx], self.gp.train_targets[idx]
+        return xmax, ymax
+
+    def update_acquisition_function(self, x):
+        raise NotImplementedError
+
+    @property
+    def acquisition_function(self):
+        return self._acquisition_function
+
+    def forward(self):
+        """Call the algorithm. """
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            y = self.acquisition_function
+            max_id = torch.argmax(y)
+            next_point = self.x[[[max_id]]]
+        return next_point
+
+
+class GPUCB(BOpt):
+    def __init__(self, gp, x, beta=2.0):
+        self.beta = beta
+        super().__init__(gp, x)
+
+    def update_acquisition_function(self, x):
+        pred = self.gp(torch.FloatTensor(x))
+        ucb = pred.mean + self.beta * pred.stddev  # Calculate UCB.
+        self._acquisition_function = ucb
+
+
+class BO_algo:
     def __init__(self):
         """Initializes the algorithm with a parameter configuration."""
         # TODO: Define all relevant class members for your BO algorithm here.
-        pass
+        # self.likelihood_f = gpytorch.likelihoods.GaussianLikelihood()
+        # self.likelihood_v = gpytorch.likelihoods.GaussianLikelihood()
+        self.likelihood_f = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise=torch.FloatTensor([0.15]))
+        self.likelihood_v = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise=torch.FloatTensor([0.0001]))
+
+        self.no_data_yet = True
+
+        self.X = []
+        self.f, self.v = [], []
 
     def next_recommendation(self):
         """
@@ -30,8 +135,8 @@ class BO_algo():
         # using functions f and v.
         # In implementing this function, you may use
         # optimize_acquisition_function() defined below.
+        return self.optimize_acquisition_function()
 
-        raise NotImplementedError
 
     def optimize_acquisition_function(self):
         """Optimizes the acquisition function defined below (DO NOT MODIFY).
@@ -61,7 +166,7 @@ class BO_algo():
         ind = np.argmax(f_values)
         x_opt = x_values[ind].item()
 
-        return x_opt
+        return np.array(x_opt).reshape(-1, 1)
 
     def acquisition_function(self, x: np.ndarray):
         """Compute the acquisition function for x.
@@ -77,9 +182,27 @@ class BO_algo():
             shape (N, 1)
             Value of the acquisition function at x
         """
-        x = np.atleast_2d(x)
-        # TODO: Implement the acquisition function you want to optimize.
-        raise NotImplementedError
+        # self.algorithm_f.update_acquisition_function(x)
+        # self.algorithm_v.update_acquisition_function(x)
+
+        """
+        beta = 2.0
+        gamma = 1.5
+
+        pred_f = self.algorithm_f.gp(torch.FloatTensor(x))
+        pred_v = self.algorithm_v.gp(torch.FloatTensor(x))
+        mean, std = pred_f.mean.detach().numpy(), pred_f.stddev.detach().numpy()
+        mean_v, std_v = pred_v.mean.detach().numpy(), pred_v.stddev.detach().numpy()
+
+        # return mean + beta * std - gamma * np.max([float(mean_v), 0.])
+        return mean + beta * std - 1e4 * np.max([float(mean_v + 1.0 * std_v)-3.5, 0.])
+        """
+        pred_f = self.algorithm_f.gp(torch.FloatTensor(x))
+        pred_v = self.algorithm_v.gp(torch.FloatTensor(x))
+        f_sample = pred_f.sample()
+        v_sample = pred_v.sample()
+
+        return f_sample - 1e4 * np.max([float(v_sample)-3.5, 0.])
 
     def add_data_point(self, x: float, f: float, v: float):
         """
@@ -94,8 +217,27 @@ class BO_algo():
         v: float
             SA constraint func
         """
-        # TODO: Add the observed data {x, f, v} to your model.
-        raise NotImplementedError
+
+        self.X.append(float(x))
+        self.f.append(float(f))
+        self.v.append(float(v))
+
+        if self.no_data_yet:
+            x, f, v = [torch.FloatTensor([i]) for i in [x, f, v]]  # convert to Tensor
+            self.no_data_yet = False
+            gp_f = GP(x, f, self.likelihood_f, type='f')
+            gp_v = GP(x, v, self.likelihood_v, type='v')
+
+            self.algorithm_f = GPUCB(gp_f, x)
+            self.algorithm_v = GPUCB(gp_v, x)
+
+        else:
+            self.algorithm_f.update_gp(np.array([x]), np.array([f]))
+            self.algorithm_v.update_gp(np.array([x]), np.array([float(v)]))
+
+        # print(f"length scale of f is {self.algorithm_f.gp.length_scale}")
+        # print(f"length scale of v is {self.algorithm_v.gp.length_scale}")
+
 
     def get_solution(self):
         """
@@ -106,8 +248,29 @@ class BO_algo():
         solution: float
             the optimal solution of the problem
         """
-        # TODO: Return your predicted safe optimum of f.
-        raise NotImplementedError
+        def objective(x):
+            x_tens = torch.FloatTensor(x)
+            v_pred = self.algorithm_v.gp(x_tens)
+            if v_pred.mean.detach().numpy() + 5 * v_pred.stddev.detach().numpy() > 3.9:
+                return 1e5 * (v_pred.mean.detach().numpy() + 5 * v_pred.stddev.detach().numpy() - 3.9)
+            return -self.algorithm_f.gp(x_tens).mean.detach().numpy()
+
+        f_values = []
+        x_values = []
+
+        # Restarts the optimization 20 times and pick best solution
+        for _ in range(20):
+            x0 = DOMAIN[:, 0] + (DOMAIN[:, 1] - DOMAIN[:, 0]) * \
+                 np.random.rand(DOMAIN.shape[0])
+            result = fmin_l_bfgs_b(objective, x0=x0, bounds=DOMAIN,
+                                   approx_grad=True)
+            x_values.append(np.clip(result[0], *DOMAIN[0]))
+            f_values.append(-result[1])
+
+        ind = np.argmax(f_values)
+        x_opt = x_values[ind].item()
+
+        return np.array(x_opt).reshape(-1, 1)
 
     def plot(self, plot_recommendation: bool = True):
         """Plot objective and constraint posterior for debugging (OPTIONAL).
@@ -116,9 +279,31 @@ class BO_algo():
         ----------
         plot_recommendation: bool
             Plots the recommended point if True.
-        """
-        pass
 
+        """
+
+        x = np.linspace(0, 10, num=1000)
+        plt.figure(figsize=(10, 6))
+        ff, vv = np.array([f(i) for i in x]), np.array([v(i) for i in x])
+        plt.plot(x, ff, 'k--', label="True F Function")
+        plt.plot(x, vv, 'k--', label="True v Function")
+        plt.scatter(self.X, self.f, color='blue', label='F')
+        plt.scatter(self.X, self.v, color='red', label='v')
+
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            pred = self.algorithm_f.gp(torch.FloatTensor(x))
+            mean = pred.mean.detach().numpy()
+            error = 2 * pred.stddev.detach().numpy()
+            pred_v = self.algorithm_v.gp(torch.FloatTensor(x))
+            mean_v = pred_v.mean.detach().numpy()
+            error_v = 2 * pred_v.stddev.detach().numpy()
+
+        plt.fill_between(x, mean - error, mean + error, lw=0, alpha=0.2, color='C0')        # out = self.algorithm_f.learner.predict_normal(x)
+        plt.fill_between(x, mean_v - error_v, mean_v + error_v, lw=0, alpha=0.2, color='salmon')
+        plt.plot(x, mean, lw=2, color='C0')
+        plt.plot(x, mean_v, lw=2, color='salmon')
+        plt.legend()
+        plt.show()
 
 # ---
 # TOY PROBLEM. To check your code works as expected (ignored by checker).
@@ -138,7 +323,7 @@ def f(x: float):
 
 def v(x: float):
     """Dummy SA"""
-    return 2.0
+    return 3.0 + 2 * np.sin(x+0.1)
 
 
 def get_initial_safe_point():
@@ -175,9 +360,10 @@ def main():
             f"shape (1, {DOMAIN.shape[0]})"
 
         # Obtain objective and constraint observation
-        obj_val = f(x) + np.randn()
-        cost_val = v(x) + np.randn()
+        obj_val = f(x) + np.random.normal(loc=0, scale=0.1)
+        cost_val = v(x) + np.random.normal(loc=0, scale=0.001)
         agent.add_data_point(x, obj_val, cost_val)
+        agent.plot()
 
     # Validate solution
     solution = agent.get_solution()
